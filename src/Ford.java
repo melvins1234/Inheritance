c45e07c06e5c71a3aa6c0be43b1b05fe3823b09d
dasdasd443 <cjvicro@gmail.com>
2021-04-08 19:35:00 +0800

    Ford_Sedan

--- src/Ford.java
+++ src/Ford.java
@@ -2,12 +2,23 @@ public class Ford extends Car{
     protected int year;
     protected int manufacturerDiscount;
 
-    public Ford(int speed, double regularPrice, String color,int fyear, int nfmanufacturerDiscount){
-        super(speed,regularPrice,color);
-        year = fyear;
-        manufacturerDiscount = nfmanufacturerDiscount;
-    }
     public double getSalePrice(){
         return super.getSalePrice() - (super.getSalePrice() * manufacturerDiscount);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getManufacturerDiscount() {
+        return manufacturerDiscount;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setManufacturerDiscount(int manufacturerDiscount) {
+        this.manufacturerDiscount = manufacturerDiscount;
     }
 }
--- src/Sedan.java
+++ src/Sedan.java
@@ -1,10 +1,14 @@
 public class Sedan extends Car{
-    protected int length;
+    protected static int length;
 
-    public Sedan(int speed, double regularPrice, String color,int slength){
-        super(speed,regularPrice,color);
-        length = slength;
+    public int getLength() {
+        return length;
     }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
     public double getSalePrice(){
         return (length > 20)? super.getSalePrice() - (super.getSalePrice() * .05): super.getSalePrice() - (super.getSalePrice() * .10);
     }
